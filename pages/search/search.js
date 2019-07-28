d1a6bc999bb584df7fa6e8e124c12865a3ddf3dd
zx <zx@gamil.com>
2019-07-28 10:08:46 +0800

    search

--- pages/search/search.js
+++ pages/search/search.js
@@ -1,4 +1,3 @@
-
 const http = require('../../utils/request.js')
 Page({
 
@@ -6,79 +5,86 @@ Page({
    * 页面的初始数据
    */
   data: {
-    inputText:'',
-    searchList:[],//关键字搜索-->商品列表
+    inputText: '',
+    searchList: [], //关键字搜索-->商品列表
+    num: 1,
+    maskVisible: false
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-
+  onLoad: function(options) {
+    console.log(getApp())
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   },
   /**自定义函数 */
   //取消
-  cancel(){
-    wx.navigateBack({
+  // 跳转购物车页面
+  toPay() {
+    wx.navigateTo({
+      url: '../submitOrder/submitOrder',
     })
   },
+  cancel() {
+    wx.navigateBack({})
+  },
   //键盘事件获取input text
-  getKeyword(e){
+  getKeyword(e) {
     this.setData({
       inputText: e.detail.value
     })
   },
   //搜索
-  search(){
+  search() {
     http.request({
       apiName: '/products',
       method: 'GET',
@@ -87,12 +93,12 @@ Page({
       },
       isShowProgress: true,
     }).then((res) => {
-      if(res.length==0){
+      if (res.length == 0) {
         wx.showToast({
           title: '无此商品',
-          icon:'none'
+          icon: 'none'
         })
-      }else{
+      } else {
         this.setData({
           searchList: res
         })
@@ -101,20 +107,54 @@ Page({
     })
   },
   //添加商品
-  add(e){
-    let productId = e.currentTarget.id;
-    http.request({
-      apiName: '/carts',
-      method: 'POST',
-      data: {
-        "product_id": productId,
-        "quantity": 1,
-      },
-      isShowProgress: true,
-    }).then((res) => {
-      wx.showToast({
-        title: '添加至购物车',
-      })
+  add(e) {
+    this.setData({
+      maskVisible: true,
+      productId: e.currentTarget.id
     })
-  }
+    //   let productId = e.currentTarget.id;
+    //   http.request({
+    //     apiName: '/carts',
+    //     method: 'POST',
+    //     data: {
+    //       "product_id": productId,
+    //       "quantity": 1,
+    //     },
+    //     isShowProgress: true,
+    //   }).then((res) => {
+    //     wx.showToast({
+    //       title: '添加至购物车',
+    //     })
+    //   })
+  },
+  handleChange({
+    detail
+  }) {
+    this.setData({
+      num: detail.value
+    })
+  },
+  handleSubmit() {
+      http.request({
+        apiName: '/carts',
+        method: 'POST',
+        data: {
+          "product_id": this.data.productId,
+          "quantity": this.data.num,
+        },
+        isShowProgress: true,
+      }).then((res) => {
+        wx.showToast({
+          title: '添加至购物车',
+        })
+        this.setData({
+          maskVisible: false
+        });
+      })
+  },
+  handleClose() {
+    this.setData({
+      maskVisible: false
+    });
+  },
 })
